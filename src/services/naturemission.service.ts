0e43366770f9ad8086569d7fd26dd0c4db7ad9d6
unknown <amraoui.djamel@gmail.com>
2018-09-27 15:23:54 +0200

    fin 14,15,16

---
+++ src/services/naturemission.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Subject, Observable } from "rxjs";
+
+import { environment } from "../environments/environment";
+import { NatureMission, Formulaire } from "../model";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { RequestOptions } from "@angular/http"
+
+const URL_BACKEND = environment.baseUrl + "naturemission";
+@Injectable({
+    providedIn: "root"
+})
+export class NatureMissionService {
+    err: string;
+    natureMissions: Array<NatureMission>;
+    constructor(private _http: HttpClient) { }
+
+    findAll(): Observable<NatureMission[]> {
+
+        return this._http.get(URL_BACKEND).pipe(
+            map((data: any[]) =>
+                data.map(natureMission =>
+                    new NatureMission(natureMission.id, natureMission.facturation, natureMission.prime, natureMission.tjm, natureMission.pourcentage, natureMission.name, natureMission.dateFin)),
+            ));
+    }
+
+    addNatureMission(formulaireAdd: Formulaire) {
+        return this._http.post(URL_BACKEND + `/new`, formulaireAdd);
+
+    }
+
+    deleteNatureMission(natureMission: NatureMission) {
+        return this._http.post(URL_BACKEND + `/delete`, natureMission);
+    }
+
+    updateNatureMission(natureMissionAModifier: NatureMission) {
+        let body = { natureMissionAModifier }
+        return this._http.post(URL_BACKEND + `/update`, natureMissionAModifier);
+    }
+}
