00ee645287865013d3694edd94f5f8fb30843e53
Dylan <dupont.cail@gmail.com>
2018-09-27 16:11:59 +0200

    #8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -11,12 +12,18 @@ import { FormsModule } from "@angular/forms";
 import { StatutConnecteService } from "./auth/statut-connecte.service";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { ListeMissionComponent } from './liste-mission/liste-mission.component';
-
-
-
+import { CalendarComponent } from './calendar/calendar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 const routes: Routes = [
   { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService] }, // /tech accessible uniquement si connecté
-  { path: 'missions', component: ListeMissionComponent, canActivate: [StatutConnecteService] }, // /mission accessible uniquement si connecté
+  { path: 'missions', component: ListeMissionComponent, canActivate: [StatutConnecteService] },
+  { path: 'calendar', component: CalendarComponent, canActivate: [StatutConnecteService] }, // /mission accessible uniquement si connecté
   { path: 'auth', component: AuthComponent },
   { path: '', redirectTo: '/tech', pathMatch: 'full' }
 
@@ -29,15 +36,24 @@ const routes: Routes = [
     TechComponent,
     AuthComponent,
     ListeMissionComponent,
+    CalendarComponent,
 
 
   ],
   imports: [
+    NgbModule,
+    CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes),
     HttpClientModule,
     MDBBootstrapModule.forRoot(),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
