dab92975c1c47218e9be2b3269c3b5ba1fffdd7f
Dylan <dupont.cail@gmail.com>
2018-10-04 18:25:14 +0200

    validation

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Collegue } from '../auth/auth.domains';
 import { Router } from '@angular/router';
-import {Observable} from "rxjs/internal/Observable";
+import { Observable } from "rxjs/internal/Observable";
 
 @Component({
   selector: 'app-menu',
@@ -11,38 +11,38 @@ import {Observable} from "rxjs/internal/Observable";
 })
 export class MenuComponent implements OnInit {
 
-  collegue:Observable<Collegue>;
+  collegue: Observable<Collegue>;
 
-  constructor(private authService:AuthService, private _authSrv:AuthService, private _router:Router) {
+  constructor(private authService: AuthService, private _authSrv: AuthService, private _router: Router) {
 
-   }
+  }
 
-   /**
-    * Action déconnecter collègue.
-    */
-   seDeconnecter() {
-     this._authSrv.seDeconnecter().subscribe(
-       value => this._router.navigate(['/auth'])
-     );
-   }
- 
+  /**
+   * Action déconnecter collègue.
+   */
+  seDeconnecter() {
+    this._authSrv.seDeconnecter().subscribe(
+      value => this._router.navigate(['/auth'])
+    );
+  }
+
 
 
   ngOnInit() {
-    this.collegue=this.authService.getCollegueConnecteObs();
+    this.collegue = this.authService.getCollegueConnecteObs();
   }
 
-// On utilise un observable pour récupérer le rôle du collègue qui se connecte
+  // On utilise un observable pour récupérer le rôle du collègue qui se connecte
 
 
-// Est connecté en tant qu'administrateur
-  isAdmin():boolean {
+  // Est connecté en tant qu'administrateur
+  isAdmin(): boolean {
     let result = false;
     this.collegue.subscribe(c => {
-      if (c && c.roles && c.roles.length>0){
-        for (let i=0; i<c.roles.length; i++){
-          if (c.roles[i]=='ROLE_ADMINISTRATEUR'){
-            result= true;
+      if (c && c.roles && c.roles.length > 0) {
+        for (let i = 0; i < c.roles.length; i++) {
+          if (c.roles[i] == 'ROLE_ADMINISTRATEUR') {
+            result = true;
           }
         }
       }
@@ -50,12 +50,26 @@ export class MenuComponent implements OnInit {
     return result;
   }
 
-// Est connecté en tant que manager
-  isManager():boolean {
+  // Est connecté en tant que manager
+  isManager(): boolean {
+    let result = false;
     this.collegue.subscribe(c => {
-      if (c && c.roles && c.roles.length>0){
-        for (let i=0; i<c.roles.length; i++){
-          if (c.roles[i]=='ROLE_MANAGER'){
+      if (c && c.roles && c.roles.length > 0) {
+        for (let i = 0; i < c.roles.length; i++) {
+          if (c.roles[i] == 'ROLE_MANAGER') {
+            result = true;
+          }
+        }
+      }
+    });
+    return result;
+  }
+  // Est connecté en tant qu'employé
+  isEmploye(): boolean {
+    this.collegue.subscribe(c => {
+      if (c && c.roles && c.roles.length > 0) {
+        for (let i = 0; i < c.roles.length; i++) {
+          if (c.roles[i] == 'ROLE_UTILISATEUR') {
             return true;
           }
         }
@@ -64,19 +78,4 @@ export class MenuComponent implements OnInit {
     });
     return false;
   }
-
-// Est connecté en tant qu'employé
-  isEmploye():boolean {
-    this.collegue.subscribe(c => {
-      if (c && c.roles && c.roles.length>0){
-        for (let i=0; i<c.roles.length; i++){
-          if (c.roles[i]=='ROLE_UTILISATEUR'){
-            return true;
-          }
-        }
-      }
-      return false;
-    });
-    return false;
-  } 
 }
